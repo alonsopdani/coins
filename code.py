248d6b4180b52a4ee0217326cf00821e13af58c8
Dani Alonso <alonsopdani@gmail.com>
2019-06-21 18:14:37 +0200

    general class created, pound deleted

--- code.py
+++ code.py
@@ -1,41 +1,35 @@
 import random
 
-class Pound:
-
-    def __init__(self, rare=False): # constructor
+class Coin:
+    def __init__(self, rare=False, clean=True, heads=True):
+        self.is_rare = rare
+        self.is_clean = clean
+        self.heads = choice
         
-        self.rare = rare
-        
-        if self.rare:
-            self.value = 1.25
+        if self.is_rare:
+            self.value = self.original_value * 1.25
         else:
-            self.value = 1
-                
-        self.colour = 'gold'
-        self.num_edges = 1
-        self.diameter = 22.5 # mm
-        self.thickness = 3.15 # mm
-        self.heads = True
+            self.value = self.original_value
+
+        if self.is_clean:
+            self.colour = self.clean_colour
+        else:
+            self.colour = self.rusty_colour
 
     def rust(self):
-        self.colour = 'greenish'
+        self.colour = self.rusty_colour
 
     def clean(self):
-        self.colour = 'gold'
+        self.colour = self.clean_colour
+
+    def __del__(self): # destructor
+        print('Coin spent')
 
     def flip(self):
         heads_options = [True, False]
         choice = random.choice(heads_options)
         self.heads = choice
 
-    def __del__(self): # destructor
-        print('Coin spent')
+class Pound:
 
-
-coin1 = Pound()
-
-print(coin1.colour)
-
-del coin1
-
-print(coin1.colour) 
+    
